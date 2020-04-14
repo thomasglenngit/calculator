a35ca0e2e6816aacce02aa3a06a9efe08c03139b
Thomas Glenn <thomasgrantglenn@mac.com>
2020-04-13 21:01:41 -0600

    add subtract, multiply, and divide to calculator variable functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 var add = function(number1, number2) {
   return number1 + number2;
-  }
+}
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+  
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
