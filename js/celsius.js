00a6ff3ab3001a2b6b966d341fcb38bb3b4d138d
Thomas Glenn <thomasgrantglenn@mac.com>
2020-04-13 22:34:37 -0600

    add celsius calculator javascript to the js file with link in the .html

---
+++ js/celsius.js
@@ -0,0 +1,9 @@
+var celsiusToFahrenheit = function(numberC) {
+  return numberC * 1.8 + 32;
+  }
+  
+  
+  var numberC = parseInt(prompt("Enter a number"));
+  
+  alert(celsiusToFahrenheit(numberC));
+  
