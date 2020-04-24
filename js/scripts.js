1e9b5c1764b210aa2be08362ec157cd47ad78c25
Thomas Glenn <thomasgrantglenn@mac.com>
2020-04-23 21:33:46 -0600

    add jquery, js, display functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 var add = function(number1, number2) {
   return number1 + number2;
-}
+};
 
 var subtract = function(number1, number2) {
   return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
-}
-  
-  
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output1").text(result);
+  });
+});
